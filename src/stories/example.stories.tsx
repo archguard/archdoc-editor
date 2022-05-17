ef7745900ddc6e0653c13aab0d1b46faa477fef5
Phodal Huang <h@phodal.com>
2022-05-17 19:43:09 +0800

    docs: update sample codes

--- src/stories/example.stories.tsx
+++ src/stories/example.stories.tsx
@@ -25,18 +25,42 @@ const Template: Story<Props> = args => <Example {...args} />;
 export const Default = Template.bind({});
 Default.args = {
   defaultValue: `
-  
-## DSL
+
+## Backend DSL
 
 \`\`\`kotlin
-@file:DependsOn("org.archguard.scanner:doc-executor:2.0.0-alpha.2")
-import org.archguard.dsl.*
-var layer = layered {
-    prefixId("org.archguard")
-    component("controller") dependentOn component("service")
-    组件("service") 依赖于 组件("repository")
+repos {
+    repo(name = "Backend", language = "Kotlin", scmUrl = "https://github.com/archguard/archguard")
+    repo(name = "Scanner", language = "Kotlin", scmUrl = "https://github.com/archguard/scanner")
 }
 \`\`\`
   
+## 架构 DSL 
+
+\`\`\`kotlin
+@file:DependsOn("org.archguard.scanner:doc-executor:2.0.0-alpha.6")
+import org.archguard.dsl.*
+
+val layer = layered {
+    prefixId("org.archguard")
+    component("interface") dependentOn component("application")
+    组件("interface") 依赖于 组件("domain")
+    component("interface") dependentOn component("infrastructure")
+
+    组件("application") 依赖于 组件("domain")
+    组件("application") 依赖于 组件("infrastructure")
+
+    组件("domain") 依赖于 组件("infrastructure")
+}
+
+graph().show(layer.relations())
+\`\`\`
+
+## Scanner DSL
+
+\`\`\`kotlin
+linter('Backend').layer()
+\`\`\`
+
   `,
 };
